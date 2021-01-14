81829f1cd17db11f307f1c5f438078fcb66de6ff
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-14 17:10:38 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog
+    attr_accessor :name
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        x = 0
+        @@all.length.times do
+            puts @@all[x].name
+            x += 1
+        end
+    end
+
+end
